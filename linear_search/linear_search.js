454c74c61d0a604217df99ad79b510dd399dce07
Mandy Chen <mandychen@alumni.ubc.ca>
2017-10-15 13:34:17 -0700

    Add linear search implementation in JavaScript

---
+++ linear_search/linear_search.js
@@ -0,0 +1,14 @@
+/**
+  * Linear search implementation in JavaScript.
+  * @param {array} arr - input array
+  * @param {any} ele - element to search for
+  * @return {number} - index of the first occurrence of @ele if it exists, -1 otherwise
+  */
+function bubbleSort(arr, ele) {
+  if (arr === null || arr.length === 0) return -1;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === ele) return i;
+  }
+  return -1;
+}
