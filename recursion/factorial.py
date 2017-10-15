35848b8e7709a19cd71d3306d398b1379534a244
gaeun917 <stila917@gmail.com>
2017-10-15 15:12:52 +0100

    factorial in python

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,11 +1,12 @@
-def factorial(num):
-    if num == 1:
-        return 1
-    else:
-        return num * factorial(num - 1)
+num = int(input("Enter a number: "))
 
-def main():
-    print(factorial(7))
+factorial = 1
 
-if __name__ == '__main__':
-    main()
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
