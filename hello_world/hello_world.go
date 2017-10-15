36d9b6e63b75898b99cabf3f730878644fd9c9d8
Swim Systems <swimsystems@openmailbox.org>
2017-10-15 12:08:13 +0200

    Add Hello World program written in Go

---
+++ hello_world/hello_world.go
@@ -0,0 +1,10 @@
+// A simple Hello World program in go.
+// Run with: 'go run hello_world.go', Build with: 'go build hello_world.go'
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello World!")
+}
