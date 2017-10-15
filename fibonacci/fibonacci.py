e3fd2dd14202b81e2564297d4aa816ffb8fd67aa
Raghvendra Sharma <raghvendrasharma41097@gmail.com>
2017-10-15 20:55:02 +0530

    Update fibonacci.py
    used proper spacing b/w operators and operands

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,9 +1,10 @@
 n=int(input("Enter the number of terms needed "))
-a,b=0,1
-print(a,b,end=" ")
+a,b = 0,1
+print a,b,
 while(n-2):
     c=a+b
     a=b
     b=c
-    print(c,end=" ")
+    print c,
     n=n-1
+
