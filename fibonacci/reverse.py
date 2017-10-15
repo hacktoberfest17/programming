6c8a0c1375847908d4cb4d5542d9a2e8d8fd354e
DamiyanVC <DamiyanVC@gmail.com>
2017-10-15 23:34:13 +0530

    removed the file

--- fibonacci/reverse.py
+++
@@ -1,8 +0,0 @@
-Number = int(input("Please Enter any number:"))
-Reverse = 0
-while(Number>0):
-    Reminder = Number%10
-    Reverse = (Reverse*10) + Reminder
-    Number = Number//10
-
-print("\n Reverse of entered number is = %d" %Reverse)
