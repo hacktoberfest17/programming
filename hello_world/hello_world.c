442e5b0b3801e1f30d3436d02445257aa6c93def
Jordan Craig <jordan.craig94@gmail.com>
2017-10-15 09:04:09 +0100

    Add Hello World in C

---
+++ hello_world/hello_world.c
@@ -0,0 +1,10 @@
+// Include the contents of stdio.h, a library with standard input and output functions such as printf().
+#include <stdio.h>
+// Execution of the program begins with the main() function.
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   // The return 0 is the exit status of the program.
+   return 0;
+}
