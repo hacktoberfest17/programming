2751490574f99049d3fc949c3160f6b51065448c
Gabriel Broilo <broilo.gabriel@gmail.com>
2017-10-15 23:34:19 +0100

    HelloWorld in Scala

---
+++ hello_world/HelloWorld.scala
@@ -0,0 +1,6 @@
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    // In case someone is interested: https://www.scala-lang.org/documentation/your-first-lines-of-scala.html
+    println("Hello, world!")
+  }
+}
