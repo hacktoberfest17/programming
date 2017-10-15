5c33a04e36c771ccba4070c8b6dc586eefe563fe
Emmanuel Amponsah <emmanuela956@gmail.com>
2017-10-15 10:11:37 -0400

    Create BubbleSort.java
    for hacktober

---
+++ Bubblesort/BubbleSort.java
@@ -0,0 +1,20 @@
+    public static int [] bubbleSort(int [] array){
+
+        int lastPosition = array.length;
+        while(lastPosition > 0){
+            int position = 1;
+
+            while(position < lastPosition){
+                if(array[position] < array[position-1]){
+                   int temp = array[position];
+                   array[position] = array[position-1];
+                   array[position-1] = temp;
+                }
+                position++;
+            }
+
+            lastPosition--;
+        }
+
+        return array;
+    }
