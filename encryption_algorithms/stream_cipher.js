3d197d607ba7272b800b0ceefe36d3d337dc8177
Anurag Tiwari <tiwari.anurag126@gmail.com>
2017-10-16 23:56:24 +0530

    Update stream_cipher.js

--- encryption_algorithms/stream_cipher.js
+++ encryption_algorithms/stream_cipher.js
@@ -1,4 +1,5 @@
-if (!process.argv[2]) console.log("Usage: node stream_cipher <key> <plaintext/ciphertext> <-h> \n Option -h means ciphertext is in hex");
+if (!process.argv[2]) 
+    console.log("Usage: node stream_cipher <key> <plaintext/ciphertext> <-h> \n Option -h means ciphertext is in hex");
 let password = Buffer(process.argv[2]);
 let enc = process.argv[4] == "-h" ? "hex": "";
 let enc2 = process.argv[4] != "-h" ? "hex": "";
