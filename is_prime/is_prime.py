4f0789f8e3301d9327202f951b551b029f0e425c
UthpalaPitawela <uthpala.isiru@gmail.com>
2017-10-16 15:06:24 -0700

    checking a number, a prime or not

---
+++ is_prime/is_prime.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+number = int(input("Enter the number: "));
+i=0;
+for j in range(1,number +1):
+ 
+    if(number%j == 0):
+        i= i+1;
+      
+
+if(i==2):
+    print(number, "is a prime number");
+else:
+    print(number, "is not a prime number");
+    
