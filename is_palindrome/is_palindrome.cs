026d989627d7681b5937a8df49921fd588e5109d
PiyushPawar17 <piyushpawar25@gmail.com>
2017-10-16 21:08:52 +0530

    Fixed issues

--- is_palindrome/is_palindrome.cs
+++ is_palindrome/is_palindrome.cs
@@ -1,6 +1,6 @@
 public class is_palindrome
 {
-	public bool is_palindrome(string str)
+	public bool isPalindrome(string str)
 	{
 		for (int i = 0, j = str.Length - 1; i < str.Length / 2; i++, j--)
 		{
