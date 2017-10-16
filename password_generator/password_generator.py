faec7f4cc130a0299e85eb8ad861f44aeb53d4f5
Hernan Olarte <hernan940730@gmail.com>
2017-10-16 16:33:59 -0500

    Add password generator based on username function

--- password_generator/password_generator.py
+++ password_generator/password_generator.py
@@ -1,3 +1,26 @@
+import random
+
+replacer = {
+    'A': '4',
+    'B': '8',
+    'E': '3',
+    'G': '6',
+    'I': '1',
+    'O': '0',
+    'S': '5',
+    'T': '7',
+    'Z': '2',
+    'a': '4',
+    'b': '8',
+    'e': '3',
+    'g': '6',
+    'i': '1',
+    'o': '0',
+    's': '5',
+    't': '7',
+    'z': '2',
+    }
+
 def test_is_valid_password ():
     assert (is_valid_password ("aA1#") == False)
     assert (is_valid_password ("asdf1234ASDF") == False)
@@ -34,4 +57,35 @@ def is_valid_password (password):
     
     return has_uppercase and has_lowercase and has_number and has_special_caracter
 
+def get_random_special_string (size = 1):
+    
+    s = ""
+    for _ in range (size):
+        s += chr (random.randint(33, 127))
+    
+    return s
+
+def generate_password (username):
+    
+    shift = ord('a') - ord('A')
+    
+    username = str (username)
+    password = ""
+    
+    for c in username:
+        if (c in replacer):
+            password += replacer[c]
+        else:
+            nc = c
+            if (c >= 'a' and c <= 'z' and random.randint(0, 2) == 1):
+                nc = chr(ord (c) - shift)
+            elif (c >= 'A' and c <= 'Z' and random.randint(0, 2) == 1):
+                nc = chr(ord (c) + shift)
+            password += nc
+    
+    while (not is_valid_password (password)):
+        password += get_random_special_string (1)
+    return password
+
 __test__()
+print (generate_password ("hernan"))
