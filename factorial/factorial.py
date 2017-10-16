e3f5949c5f59a414f0031fa24f1e787ad7dbcf81
Arthur Fortes <fortes.arthur@gmail.com>
2017-10-16 15:59:01 -0200

    Fix bug
    Fixing bug (the code wasn't running)

--- factorial/factorial.py
+++ factorial/factorial.py
@@ -1,4 +1,8 @@
-#!/bin/env python
+"""
+
+Program to calculate factorial using Python (2.7 or 3)
+
+"""
 
 def factorial(n):
     return 1 if (n < 1) else n * factorial(n-1)
@@ -8,4 +12,4 @@ user_input = int(input("Enter a number: "))
 if user_input < 0:
     print("Sorry, factorial does not exist for negative numbers.")
 else:
-    print("The factorial of {} is {}".format(user_input, factorial(user_input))
+    print("The factorial of " + str(user_input) + " is " + str(factorial(user_input)))
