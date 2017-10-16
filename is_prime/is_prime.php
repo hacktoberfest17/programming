56980dd709c6f7e3fc2220f578401ba001e0a3dd
UthpalaPitawela <uthpala.isiru@gmail.com>
2017-10-16 16:23:33 -0700

    is_prime in php

---
+++ is_prime/is_prime.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+ <form action="" method="post">
+  Enter the numer: <input type="" name="number"><br>
+
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
+
+<?php
+$number = $_POST['number'];
+
+$j=0;
+for($i=1;$i<=$number;$i++){
+  if($number % $i == 0){
+    $j++;}
+}
+
+if($j==2){
+ print("prime");
+}else{
+ print("not a prime");
+}
+?>
