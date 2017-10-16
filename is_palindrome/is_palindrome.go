0fccc8784621a616619a32991aba05143c58ff07
Mrigank Krishan <mrigankkrishan@gmail.com>
2017-10-16 15:12:28 +0530

    Create is_palindrome.go

---
+++ is_palindrome/is_palindrome.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+ "fmt"
+)
+
+func main() {
+	//check function
+	if(isPalindrome("mam")){
+		fmt.Println("Palindrome checker worked!")
+	}else{
+		fmt.Println("Oops! there is a problem")
+	}
+ 
+}
+
+
+func isPalindrome(s string) bool {
+	mid := len(s) / 2
+	last := len(s) - 1
+	for i := 0; i < mid; i++ {
+		if s[i] != s[last-i] {
+			return false
+		}
+	}
+	return true
+}
