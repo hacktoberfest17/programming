e2ed754d14eb11736208b0566fae70beb35e1988
UthpalaPitawela <uthpala.isiru@gmail.com>
2017-10-16 16:34:36 -0700

    Made changes in prime php code

--- is_prime/is_prime.php
+++ is_prime/is_prime.php
@@ -1,25 +1,30 @@
 <html>
-<body>
- <form action="" method="post">
-  Enter the numer: <input type="" name="number"><br>
-
-<input type="submit" value="Submit">
-</form>
-</body>
+  <body>
+    <form action="" method="post">
+      Enter the numer: <input type="" name="number"><br>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
 </html>
 
 <?php
 $number = $_POST['number'];
 
 $j=0;
-for($i=1;$i<=$number;$i++){
-  if($number % $i == 0){
-    $j++;}
+for($i=1;$i<=$number;$i++)
+{
+  if($number % $i == 0)
+  {
+    $j++;
+  }
 }
 
-if($j==2){
+if($j==2)
+{
  print("prime");
-}else{
+}
+else
+{
  print("not a prime");
 }
 ?>
