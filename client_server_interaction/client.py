17e4c447615b1d5660633256ce3f865e488beba0
v3nturetheworld <pheinenjr@gmail.com>
2017-10-16 14:40:32 -0600

    Basic client-server interaction example using python

---
+++ client_server_interaction/client.py
@@ -0,0 +1,29 @@
+import socket, sys
+
+class tcp_client:
+    '''
+    A simple tcp_client class using sockets
+    '''
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def ping(self):
+        try:
+            self.socket_client.connect((self.host, self.port))
+            self.socket_client.sendall(bytes("Ping!\n", "utf-8"))
+
+            self.response = str(self.socket_client.recv(10), "utf-8")
+            print(f"[Response From Server]: {self.response}")
+        except Exception as e:
+            print(f"[Error!]: {e}")
+        finally:
+            self.socket_client.close()
+
+if __name__ == "__main__":
+    # A simple example of using the client
+    client = tcp_client("localhost", 9998)
+    client.ping()
+
---
+++ client_server_interaction/server.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+# A simple TCP Server
+
+import socketserver
+
+class tcp_handler(socketserver.BaseRequestHandler):
+    '''
+    A simple Handler Class for tcp socket servers
+    '''
+
+    def handle(self):
+        # Note: Raw strings can't be sent
+        self.client_data = self.request.recv(10).strip().decode("utf-8")
+        print(f"[{self.client_address[0]}]: {self.client_data}")
+        self.request.sendall("Pong!".encode("utf-8"))
+
+class tcp_server:
+    '''
+    A simple TCP server using the socketserver.TCPServer class
+    '''
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def start(self):
+        try:
+            # Allows us to re-use ports so we don't have to open a new port every time we run the server program
+            socketserver.TCPServer.allow_reuse_address = True
+            self.server = socketserver.TCPServer((self.host, self.port), tcp_handler)
+            self.server.serve_forever()
+        except Exception as e:
+            print(f"[Error!]: {e}")
+        finally:
+            self.server.server_close()
+
+if __name__ == "__main__":
+    # Basic Example
+    server = tcp_server("localhost", 9998)
+    server.start()
