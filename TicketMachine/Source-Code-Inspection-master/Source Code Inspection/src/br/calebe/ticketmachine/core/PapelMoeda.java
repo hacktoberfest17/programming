a61a9ceb740904f148a0968cf01932b8b7c03525
wellyogui <well_yogui@hotmail.com>
2017-10-16 20:46:46 -0200

    Ticket-Machine

---
+++ TicketMachine/Source-Code-Inspection-master/Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -0,0 +1,24 @@
+package br.calebe.ticketmachine.core;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+public class PapelMoeda {
+
+    protected int valor;
+    protected int quantidade;
+
+    public PapelMoeda(int valor, int quantidade) {
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+}
