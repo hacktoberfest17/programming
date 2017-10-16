f98780c8a4f6ba5b0512671464ebe19c78176bea
Danny Grove <danny@drgrovellc.com>
2017-10-16 12:18:55 -0700

    Add fibonacci for JavaScript

---
+++ fibonacci/fibonacci.js
@@ -0,0 +1,26 @@
+const fibs = new Map();
+fibs.set(1, 1);
+fibs.set(2,1);
+
+/**
+ * Get fibonacci number at a particular place.
+ *
+ * @param {number} place
+ * @return {number}
+ */
+function fib(place) {
+  if (fibs.has(place)) {
+    return fibs.get(place);
+  }
+  const f = fib(place  - 2) + fib(place - 1);
+  fibs.set(place, f);
+  return f;
+}
+
+function main() {
+  for(let i = 1; i < Math.random() * 1000; i++) {
+    console.log(fib(i));
+  }
+}
+
+main();
