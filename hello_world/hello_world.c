b329e68ef6be16a98a7a67da97d1cdf3c3176fb5
Ambuj <thecoderguy@outlook.com>
2017-10-16 22:28:53 +0530

    int to void and getch() included

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -1,9 +1,10 @@
 // Include the contents of stdio.h, a library with standard input and output functions such as printf().
 #include <stdio.h>
+#include <conio.h>
 // Execution of the program begins with the main() function.
-int main()
+void main()
 {
    // printf() displays the string inside quotation
    printf("Hello, World!");
-   return 0;
+   getch();
 }
