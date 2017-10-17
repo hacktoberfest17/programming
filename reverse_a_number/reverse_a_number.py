6470ee5db28a162849467a1d6755235f598a8910
AnupKumarGupta <reachanupgupta@gmail.com>
2017-10-17 22:09:00 +0530

    Adds Reverse a number in Python

---
+++ reverse_a_number/reverse_a_number.py
@@ -0,0 +1,5 @@
+num = int(input("Please Enter any Number: "))  # let this be our number
+reverse_num = str(num)[::-1]
+ 
+print(int(reverse_num))  # print after converting the string back to integer
+
