247c2cf7358d2603e575bf85ae01e0fa9553234a
Oscar Johansson <1994.oscar.j@gmail.com>
2017-10-17 15:09:59 +0200

    Move Bubble_sort.py to sorting
    Also rename to fit with convention

--- sorting/bubble_sort/bubble_sort.py
+++ sorting/bubble_sort/bubble_sort.py
@@ -9,6 +9,7 @@ def bubble_sort(alist):
                 alist[j+1] = temp
 
 if __name__ == '__main__':
+    
     #Sample data for sorting.
     ALIST = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     bubble_sort(ALIST)
