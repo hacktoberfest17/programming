bc304c556578d590c32e47b818ad8c414a59bee1
AnupKumarGupta <reachanupgupta@gmail.com>
2017-10-17 22:25:40 +0530

    Adds GCD of two numbers inC

---
+++ gcd/gcd.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+// Recursive function to return gcd of a and b
+int gcd(int a, int b)
+{
+    // Everything divides 0 
+    if (a == 0 || b == 0)
+       return 0;
+ 
+    // base case
+    if (a == b)
+        return a;
+ 
+    // a is greater
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+ 
+// Driver program to test above function
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("GCD of %d and %d is %d ", a, b, gcd(a, b));
+    return 0;
+}
+
+
