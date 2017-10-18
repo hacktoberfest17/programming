8674e3e9901aee79d7af3a0b97e6f97cc60812f5
Ganesh Patro <30416891+ganeshpatro321@users.noreply.github.com>
2017-10-18 14:02:07 +0530

    new lines deleted

--- sorting/bucketsort/bucket_sort.py
+++ sorting/bucketsort/bucket_sort.py
@@ -9,8 +9,6 @@ def bsort(n1,n2,b,n,bucket,r):
     for i in  range(b):
         ar.extend(bucket[i])
     return ar
-
-
 def isort(f,ar):
     for j in range (1,f):
             k = j-1
@@ -20,12 +18,6 @@ def isort(f,ar):
                 k -= 1
             ar[k+1] = key
         return ar
-
-
-
-
-
-
 n1,n2,b = raw_input().strip().split(' ')
 n1,n2,b = [int(n1),int(n2),int(b)]
 n = list(map(int,raw_input().strip().split(' ')))
