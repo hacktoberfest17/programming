3dedf69aeec55f76577859e24eb1c290cd0a1cea
Prashant goyal <prashantgoyal38@yahoo.com>
2017-10-19 23:35:05 +0530

    linear regression updated

--- machine_learning/linear_regression/linear_regression.py
+++ machine_learning/linear_regression/linear_regression.py
@@ -1,16 +1,19 @@
+# manual algebrical method
 import numpy as np
 import sys
 print(sys.version)
 
-x = np.array([[1, 2, 1], [2, 3, 1], [3, 4, 1], [6, 8, 1]])
-y = [4, 7, 11, 21]
+x = np.array([[1, 2, 1], [2, 3, 1], [3, 4, 1], [6, 8, 1], [14, 6, 1]])
+y = [4, 7, 11, 21, 25]
 
 x_tr = np.array(x).T
 b = np.dot(np.dot(np.linalg.inv(np.dot(x_tr,x)), x_tr) ,y)
 print(b)
 
-x = np.array([[1, 2], [2, 3], [3, 4], [6, 8]])
+
+# sklearn method
 from sklearn.linear_model import LinearRegression
 lr = LinearRegression()
+x = np.array([[1, 2], [2, 3], [3, 4], [6, 8], [14, 6]])
 lr.fit(x, y)
 print(lr.coef_, lr.intercept_)
