18ed56676aeb2c04698372eaa6008475bedaf612
Allen Computer Science Club <taylor_hudson@allenisd.org>
2017-10-20 06:45:53 -0500

    Update fibonacci.cpp
    Modifiying this C++ program to implement the fibonacci operation in a space efficient, non-recursive manner that will work beyond long long

--- fibonacci/cpp/fibonacci.cpp
+++ fibonacci/cpp/fibonacci.cpp
@@ -1,5 +1,54 @@
+// MOD AUTHOR:  Taylor Hudson
+// GITHUB    :  https://github.com/allencompsci
+// DATE      :  October 20, 2017
+// DESCRITION:  This C++ program implements the fibonacci operation in a space efficient, non-recursive manner and works beyond long long
+
 #include <iostream>
+#inlcude <string>
 using namespace std;
+
+string add(string a, string b) { // ADDED 
+	string temp = "";
+	while ((int)a.length() < (int)b.length()) {
+		a = "0" + a;
+	}
+	while ((int)b.length() < (int)a.length()) {
+		b = "0" + b;
+	}
+	int carry = 0;
+	for (int i = a.length() - 1; i >= 0; i--) {
+		char val = (char)(((a[i] - 48) + (b[i] - 48)) + 48 + carry);
+		if (val > 57) {
+			carry = 1;
+			val -= 10;
+		}
+		else {
+			carry = 0;
+		}
+		temp = val + temp;
+	}
+	if (carry != 0) {
+		temp = "1" + temp;
+	}
+	while (temp[0] == '0' && temp.length() != 1) {
+		temp = temp.substr(1);
+	}
+	return temp;
+}
+
+string fib_Accurate(long long n) { // ADDED
+	string tmp = "";
+	string fibMinus1 = "1";
+	string fibMinus2 = "0";
+	string comma = ", ";
+	for (long long i = 2; i < n; i++) {
+		tmp = add(fibMinus1, fibMinus2);
+		fibMinus2 = fibMinus1;
+		fibMinus1 = tmp;
+	}
+ return tmp;
+}
+
  int fib(int n, int a = 0, int b = 1)
 {
     if (n == 0)
@@ -12,8 +61,9 @@ using namespace std;
 int main()
 {
     cout<<"Enter the value :-";
-    int n;
+    long long n; //Modified to accept larger values 
     cin>>n;
-    cout << "fib(" << n << ") = "<< fib(n) << endl;
+    cout << "fib(" << n << ") = " << fibAccurate(n) << '\n'; // endl flushes new line character is slightly faster, but more efficent in multiple uses.
+    cout << "fib(" << n << ") = "<< fib((int)n) << endl; // Modified to send only int in.
     return 0;
 }
