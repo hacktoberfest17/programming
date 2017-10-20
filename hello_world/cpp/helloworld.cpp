1000c0207b0520f87c49f01f5332732dba4adbdc
SHAKTI SINGH <mohittanwar2323@gmail.com>
2017-10-20 16:56:05 +0530

    another way to print hello world
    without writing in main function printing hello world using struct

---
+++ hello_world/cpp/helloworld.cpp
@@ -0,0 +1,13 @@
+//another way to print hello world
+#include<iostream>
+using namespace std;
+struct prog 
+{
+
+       prog()
+       {
+       cout<<"hello world";
+       }
+} p;
+int main(){
+}
