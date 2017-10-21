8432395143ce834b1d90adb3d1bbede92981fdcb
davidz <davidz@USLC02R83CZFVH8.local>
2017-10-21 13:35:42 -0400

    fibonacci separate file

--- recursion/js/factorial.js
+++ recursion/js/factorial.js
@@ -6,8 +6,4 @@ function factorial (n) {
 console.log(factorial(10)) // 3,628,800
 
 
-function fibonacci (n) {
-	if (n === 0) { return 0 }
-	if (n === 1) { return 1 }
-	return fibonacci(n - 1) + fibonacci(n - 2)
-}
+
