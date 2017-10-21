1a1d00a62329dc4cd1d3ddd8bb7ccde6ea56d82f
Rajat Kumar Gupta <rajatcube@gmail.com>
2017-10-21 14:11:49 +0530

    made a new directory

---
+++ mathematical_operations/math_operations.java
@@ -0,0 +1,31 @@
+public class math_operations {
+    public static void main(String args[]){
+
+        int input1;
+        int input2;
+        int product;
+        int sum;
+        int difference;
+        double quotient;
+        int remainder;
+
+        input1 = 10;
+        input2 = 5;
+
+        product = input1*input2;
+        sum = input1+input2;
+        difference = input1 - input2;
+        quotient = input1/input2;
+        remainder = input1%input2;
+
+
+        System.out.print("Input 1 = "+input1);
+        System.out.print("\nInput 2 = "+input2);
+        System.out.print("\nProduct = "+product);
+        System.out.print("\nSum = "+sum);
+        System.out.print("\nDifference = "+difference);
+        System.out.print("\nQuotient = "+quotient);
+        System.out.print("\nRemainder = "+remainder);
+
+    }
+}
