cb412620fd2f7cb4ddfd3db42d9cf66087fc0193
hsaini734 <hsaini734@gmail.com>
2017-10-21 18:14:36 +0530

    updated directory structure and taken inpun using scanner

--- string_palindromic_substring_count/java/StringPalindromicSubstringCount.java
+++ string_palindromic_substring_count/java/StringPalindromicSubstringCount.java
@@ -1,10 +1,12 @@
-package lecture13;
+import java.util.*;
 
 public class StringPalindromicSubstringCount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(countPalindromes("abbab"));
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter String");
+		System.out.println(scn.next());
 	}
 
 	public static int countPalindromes(String str){
