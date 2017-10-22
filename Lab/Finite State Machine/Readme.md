250ac6047408ca4d8e08fff2e5e3df0b18941590
Rohit Das <rohit.das950@gmail.com>
2017-10-22 23:11:36 +0530

    Lab/*: Added files written in verilog HDL
    Signed-off-by: Rohit Das <rohit.das950@gmail.com>

---
+++ Lab/Finite State Machine/Readme.md
@@ -0,0 +1,5 @@
+# Finite State Machine
+
+The following Verilog code is the implementation of the FSM given below:-
+
+
---
+++ Lab/Unspecified/csa.v
@@ -0,0 +1,8 @@
+i am a bad boy .I want to kill sumitro ranodeep saha and samarjit naskar
+keu day?
+na age dito
+K? sagorika?
+na ranodeep gay 
+bujhlam keno sagorika khisti mere break up koreche
+lol^ infility
+
