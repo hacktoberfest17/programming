d2188c7fa92f4ecddd7d5a3292fadc709a6a1ba1
Brandon Azer <azerbran@msu.edu>
2017-10-23 11:39:22 -0400

    Create isHalloween.swift

---
+++ halloween/isHalloween.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let date = Date()
+let calendar = Calendar.current
+let month = calendar.component(.month, from: date)
+let day = calendar.component(.day, from: date)
+
+if (month == 10 && day == 31){
+    print("Happy Halloween!")
+}
+    
+else {
+    print("It's October, but today is not Halloween!")
+}
+
