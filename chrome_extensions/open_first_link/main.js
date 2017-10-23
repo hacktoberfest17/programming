a278f6fbb25a67fbdcd782434d642f1140125dc8
Bharat Saraswat <bsaraswat@gmail.com>
2017-10-23 14:42:38 +0530

    initial commit for open first link chrome extension

---
+++ chrome_extensions/open_first_link/main.js
@@ -0,0 +1,10 @@
+/**
+ * Listens for the app launching, then creates the window.
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+  console.log("LOAD!");
+  window.open(document.querySelector('a').href);
+});
