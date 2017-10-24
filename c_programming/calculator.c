9a59bac3bdffe5bd4cf1446328a88268b3742db2
moses-lee <30307518+moses-lee@users.noreply.github.com>
2017-10-24 00:53:25 -0700

    added calculator.c (#1133)

---
+++ c_programming/calculator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+void main(){
+
+//declarations
+double num1, num2;
+char op;
+
+printf("Enter the calculation:\n");
+scanf("%lf%c%lf", &num1,&op,&num2);
+
+switch(op){
+    case '+':
+        printf("=%lf\n", num1+num2);
+        break;
+    case '-':
+        printf("=%lf\n", num1-num2);
+        break;
+    case '*':
+        printf("=%lf\n", num1*num2);
+        break;
+    case '/':
+        if(num2==0){
+            printf("Division by zero error!\n");
+        }else{
+            printf("=%lf\n", num1/num2);
+        }
+        break;
+    case '%':
+        printf("=%l\n", (long)num1%(long)num2);
+        break;
+    default: printf("Invalid Operator\n");
+}
+
+}
+
+
+
