f6a2461485152c777148a9e7c58c71054adff3ed
Himanshu Awasthi <johim9493@gmail.com>
2017-10-24 22:12:49 +0530

    Python Socket Program

---
+++ Socket_Programming/socket_python/client.py
@@ -0,0 +1,16 @@
+# import socket module
+import socket               
+ 
+# Create a socket object
+s = socket.socket()         
+  
+# Define the port on which you want to connect
+port = 12345               
+   
+# connect to the server on local computer
+s.connect(('127.0.0.1', port))
+    
+# receive data from the server
+print s.recv(1024)
+# close the connection
+s.close()
