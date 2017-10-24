19f0ce784a94386464ba471ffdc6ea6b26b9b795
shubham thakur <sbmthakur@gmail.com>
2017-10-24 21:28:56 +0530

    Simplest C program:
    As you might have guessed, the program does nothing. The point of adding
    this program is to elucidate various steps involved in the compilation
    of a C program. Try playing with it various gcc options and observe the
    output.

---
+++ simplest_C_program/simplest_C_program.C
@@ -0,0 +1,9 @@
+/*
+ * Simpest C program:
+ * 
+ * Try running this program with various gcc options:
+ *    gcc -E simplest_C_program.C -o output.e
+ *    gcc -S simplest_C_program.C -o output.S
+ *    gcc -c simplest_C_program.C -o output
+ */
+main() {}
