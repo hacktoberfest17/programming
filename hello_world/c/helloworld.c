4999bb7f5d4fa8113627ce8cef9f769b39752a40
dynamicdebian <33061010+dynamicdebian@users.noreply.github.com>
2017-10-24 19:57:24 +0530

    updated hello_world.c (#1178)
    printed hello world in C

--- hello_world/c/helloworld.c
+++ hello_world/c/helloworld.c
@@ -1,12 +1,10 @@
 // Include the contents of stdio.h, a library with standard input and output functions such as printf().
 #include <stdio.h>
-#include <conio.h>
 // Execution of the program begins with the main() function.
 int main(void)
 {
    // printf() displays the string inside quotation
    printf("Hello, World!");
-   getch();
-
+   
    return 0; /* Exit the program with return code 0 (success) */
 }
