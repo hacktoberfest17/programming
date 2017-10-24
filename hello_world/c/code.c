2bc735e84e97e93a9994f3f5e52ec595fd339146
Divy Ranjan <divyranjan90@gmail.com>
2017-10-24 12:31:36 +0000

    Add files via upload

---
+++ hello_world/c/code.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+ //for including function printf() from the header file stdio.h(standard input-output)
+int main()
+{
+	printf("Hello, World! \n"); //'\n' is the newline character to end a line and move the pointer to a new line
+	return 0;
+}
