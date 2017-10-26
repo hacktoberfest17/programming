39f3d74e7ed2fb3490c3258432c997031d150dc1
Yu-Hsi Chiang <arbuz.tw@gmail.com>
2017-10-26 22:34:23 +0800

    Add merge sort implementation in Haskell.

---
+++ sorting/merge_sort/merge_sort.hs
@@ -0,0 +1,13 @@
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+  | x <= y = x : merge xs (y:ys)
+  | otherwise = y : merge (x:xs) ys
+
+mergeSort :: Ord a => [a] -> [a]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs = (mergeSort ys) `merge` (mergeSort zs) 
+  where n = length xs
+        (ys, zs) = splitAt (n `div` 2) xs
