0b60e7dace7fee0e4c5cdfd8e7294fa00f78a8f2
Yash Mathur <mathuryash5@gmail.com>
2017-10-28 04:41:50 +0530

    Bubble Sort in GoLang

---
+++ sorting/bubble_sort/bubble_sort.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {  
+  var numbers []int = []int{5, 4, 2, 3, 1, 0}
+  fmt.Println("Unsorted:", numbers)
+
+  bubbleSort(numbers)
+  fmt.Println("Sorted:", numbers)
+}
+
+func bubbleSort(numbers []int) {  
+  var N int = len(numbers)
+  var i int
+  for i = 0; i < N; i++ {
+    sweep(numbers)
+  }
+}
+
+func sweep(numbers []int) {  
+  var N int = len(numbers)
+  var firstIndex int = 0
+  var secondIndex int = 1
+
+  for secondIndex < N {
+    var firstNumber int = numbers[firstIndex]
+    var secondNumber int = numbers[secondIndex]
+
+    if firstNumber > secondNumber {
+      numbers[firstIndex] = secondNumber
+      numbers[secondIndex] = firstNumber
+    }
+
+    firstIndex++
+    secondIndex++
+  }
+}
