6fcda12e416849b61034b72d66a67eb96e6d50e3
UmaRamya <umaramyavigrahala@gmail.com>
2017-10-30 22:02:26 +0530

    Age from birthdate in python
    Added the code might make code simple, replace the code with stars*

--- beginner_programming/age_counter/python/count_age_of_person.py
+++ beginner_programming/age_counter/python/count_age_of_person.py
@@ -1,12 +1,20 @@
 from datetime import datetime
 
 
-def calculate_age(date_obj):
+//def calculate_age(date_obj):
     current_date = datetime.now()
     age = current_date - date_obj
     print("Current age (days): {}".format(age.days))
-    print("Current age (years): {0:.2f}".format(age.days / 365))
-
+    print("Current age (years): {0:.2f}".format(age.days / 365))//
+    
+***
+def age(birthday):
+    today = date.today()
+    y = today.year - birthday.year
+    if today.month < birthday.month or today.month == birthday.month and today.day < birthday.day:
+        y -= 1
+    return y    
+***
 
 if __name__ == "__main__":
     age_string = input("Please insert birth year (mm/dd/yyyy): ")
