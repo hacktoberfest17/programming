2dd4367e98f531b3f4dd5e08f5f9b67de8547e04
dulangaheshan <33221629+dulangaheshan@users.noreply.github.com>
2017-10-30 16:55:07 +0530

    Update factorial_in_c.c

--- factorial/C/factorial_in_c.c
+++ factorial/C/factorial_in_c.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+int factorial(int n)
+int main()
+  int n;
+scanf("%d",&n);
+printf("%d",factorial(int n));
+
+int multiplyNumbers(int n)
+{
+    if (n >= 1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
+}
+
 
