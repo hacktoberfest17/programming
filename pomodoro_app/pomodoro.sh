bb3851d697f492bbdf7c8483ac1d3f57074adc4d
Tommaso Montefusco <Tommaso.Montefusco@hotmail.it>
2017-10-30 16:16:10 +0100

    add pomodoro app in bash

---
+++ pomodoro_app/pomodoro.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+wseconds=${1:-25}*60;
+pseconds=${2:-wseconds/300}*60;
+
+while true; do
+	date1=$((`date +%s` + $wseconds));
+	while [ "$date1" -ge `date +%s` ]; do
+  		echo -ne "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r";
+	done
+	notify-send "Break" "Time to walk and rest";
+	read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n';
+	date2=$((`date +%s` + $pseconds));
+	while [ "$date2" -ge `date +%s` ]; do
+  		echo -ne "$(date -u --date @$(($date2 - `date +%s` )) +%H:%M:%S)\r";
+	done
+	notify-send "Work" "Time to get back to work";
+	read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n';
+done
