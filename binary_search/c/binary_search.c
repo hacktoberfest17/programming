fe1a2ab61e87aaf733b6add4e3caf7365d516f85
AgentJ08 <32589174+AgentJ08@users.noreply.github.com>
2017-10-31 01:42:35 +0530

    Update binary_search.c

--- binary_search/c/binary_search.c
+++ binary_search/c/binary_search.c
@@ -2,17 +2,18 @@
 
 int binarySearch(int arr[], int l, int r, int x)
 {
+   int mid;
    if (r >= l)
    {
-        int mid = l + (r - l)/2;
+        mid = l + (r - l)/2;
  
         if (arr[mid] == x)  return mid;
  
-        if (arr[mid] > x) return binarySearch(arr, l, mid-1, x);
+        else if (arr[mid] > x) return binarySearch(arr, l, mid-1, x);
  
-        return binarySearch(arr, mid+1, r, x);
+        else return binarySearch(arr, mid+1, r, x);
    }
-   return -1;
+   else return -1;
 }
  
 int main(void)
