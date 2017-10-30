7b0c91e9175d42df9cd968384c7eab0d1ace4ca2
Abe Massry <amassry@linode.com>
2017-10-30 13:09:47 -0400

    fix break-statement.sh

--- shell_scripting/break-statement.sh
+++ shell_scripting/break-statement.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 #while loop in action
 count=1
-while [ $count -le 10 ]
+while [[ $count -le 10 ]]
 do
-	if [ $count -eq 4 ]
+	if [[ $count -eq 4 ]]
 	then
 		break
 	fi
 	echo $count
-	count=`expr $count +1`
+	count=$((count+1))
 done
 
 
