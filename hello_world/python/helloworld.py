84dba50e30861978b885096e94db16abd86badea
troe27 <tilman.roenneburg@googlemail.com>
2017-10-31 20:56:58 +0100

    added the if __main__ thingy

--- hello_world/python/helloworld.py
+++ hello_world/python/helloworld.py
@@ -1,12 +1,18 @@
-import os
-import re
-import subprocess
-pattern = re.compile("[Hh][Ee][Ll][Ll][Oo]_*[wW][Oo][Rr][Ll][Dd]+\.py")
-a = os.listdir(".")
-for i in  a:
-    if pattern.match(i) and  i != str(os.path.basename(__file__)):
+
+def main():
+    import os
+    import re
+    import subprocess
+
+    pattern = re.compile("[Hh][Ee][Ll][Ll][Oo]_*[wW][Oo][Rr][Ll][Dd]+\.py")
+    a = os.listdir(".")
+    for i in  a:
+        if pattern.match(i) and  i != str(os.path.basename(__file__)):
             subprocess.call(["python", i])
             break
-    else:
-        print "Hello World!"
-        exit(0)
+        else:
+            print "Hello World!"
+            exit(0)
+
+if __name__ == "__main__":
+    main()
