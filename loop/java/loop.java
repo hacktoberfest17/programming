72c46427be586cb8125c494f698f6e6875c73f0c
nspiumal <33251429+nspiumal@users.noreply.github.com>
2017-10-31 16:06:16 +0530

    Create loop.java

---
+++ loop/java/loop.java
@@ -0,0 +1,9 @@
+class ForLoopExample3 {
+    public static void main(String args[]){
+         int arr[]={2,11,45,9};
+         //i starts with 0 as array index starts with 0 too
+         for(int i=0; i<arr.length; i++){
+              System.out.println(arr[i]);
+         }
+    }
+}
