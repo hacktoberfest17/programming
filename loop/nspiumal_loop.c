d688070517a7fb51e9fdbeff3bb5d4bb0f8a7896
nspiumal <33251429+nspiumal@users.noreply.github.com>
2017-10-31 15:58:40 +0530

    Create nspiumal_loop.c

---
+++ loop/nspiumal_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int num, count, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    // for loop terminates when n is less than count
+    for(count = 1; count <= num; ++count)
+    {
+        sum += count;
+    }
+    printf("Sum = %d", sum);
+}
