75b8ca9303d0fa6959c74c0f80101f6f2640ed00
Nitin Gupta <nitingupta2906@gmail.com>
2017-11-16 15:22:18 +0530

    Update hello_world.java

--- hello_world/java/hello_world.java
+++ hello_world/java/hello_world.java
@@ -1,10 +1,8 @@
 public class HelloWorld {
-    @Override
-    public static void println() {
-        System.out.println("Hello Java World");
-    }
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
     }
+
 }
