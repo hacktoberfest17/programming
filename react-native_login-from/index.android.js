825bac16d0fd4a36738205a13782407af9b10a02
Mayank Kulkarni <31683839+mayankk245@users.noreply.github.com>
2018-10-16 15:07:39 +0530

    React-naive Login form Compnent

---
+++ react-native_login-from/index.android.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import {AppRegistry,Text,View} from 'react-native';
+import App from './App.js';
+import {name as appName} from './app.json';
+
+
+
+AppRegistry.registerComponent(appName, () => App);
