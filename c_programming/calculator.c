720e8fc4a5a345b62e15c102d83e92a1d309818c
kushan2018 <38235669+kushan2018@users.noreply.github.com>
2018-10-21 11:34:14 +0530

    Update calculator.c

--- c_programming/calculator.c
+++ c_programming/calculator.c
@@ -36,4 +36,32 @@ switch(op){
 }
 
 
+##  Half pyramid alphabets programming 
+
+
+
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    char input, alphabet = 'A';
+
+    printf("Enter the uppercase character you want to print in last row: ");
+    scanf("%c",&input);
+
+    for(i=1; i <= (input-'A'+1); ++i)
+    {
+        for(j=1;j<=i;++j)
+        {
+            printf("%c", alphabet);
+        }
+        ++alphabet;
+
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
 
