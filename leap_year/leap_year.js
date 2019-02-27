b984b6bec762348eed8b24cced5f4d41a863880a
BuildTools <unconfigured@null.spigotmc.org>
2019-02-27 16:49:08 -0600

    Fixes #2027

---
+++ leap_year/leap_year.js
@@ -0,0 +1,15 @@
+function isLeapYear() {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
