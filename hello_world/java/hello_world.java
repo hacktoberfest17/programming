5bb075d823f073d8eb46c76165207665960fe3a0
debarghya472 <54908977+debarghya472@users.noreply.github.com>
2019-10-02 14:46:32 +0530

    updated hello world

--- hello_world/java/hello_world.java
+++ hello_world/java/hello_world.java
@@ -5,6 +5,6 @@ public class HelloWorld {
     }
     
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("HELLO WORLD!!");
     }
 }
