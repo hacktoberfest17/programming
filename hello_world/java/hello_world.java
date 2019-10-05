e313c958ef3ce8ea0bc3b9bbf258e1429d22d4f8
krishnaagarwal1feb <krishnaagarwal199038@gmail.com>
2019-10-06 01:09:41 +0530

    hello world in java without object creation

--- hello_world/java/hello_world.java
+++ hello_world/java/hello_world.java
@@ -1,10 +1,11 @@
-public class HelloWorld {
-    @Override
-    public static void println() {
-        System.out.println("Hello Java World");
-    }
-    
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+/* This is a simple Java program. 
+FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+	// Your program begins with a call to main(). 
+	// Prints "Hello, World" to the terminal window. 
+	public static void main(String args[]) 
+	{ 
+		System.out.println("Hello, World"); 
+	} 
+} 
