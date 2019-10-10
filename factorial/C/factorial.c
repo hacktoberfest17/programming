b4583721328d25d45c0915cecd8d34eb799b894f
Afroz quraishi <43930348+Afroz78692@users.noreply.github.com>
2019-10-10 10:56:51 +0530

    Very simple factorial programme

--- factorial/C/factorial.c
+++ factorial/C/factorial.c
@@ -1,28 +1,16 @@
 #include<stdio.h>
-#include<conio.h>
- 
-int result[1000] = {0};
-
-int fact_dp(int n)
-{
-    int i;
-    result[0] = 1;
-    for (i = 1; i <= n; ++i) 
-    {
-    	result[i] = i * result[i - 1];
-    }
-    
-    return result[n];
-}
-
 int main()
 {
-	clrscr();
-	int n;
-	printf("Enter a number : ");
+	int a,i,n,fact=1;
 	scanf("%d",&n);
-	if(n<=0)
-		printf("1\n");
-	else
-		printf("\nFactorial of the given number is :%d",fact_dp(n));
+	if(n>=0)
+	{
+for(i=1;i<=n;i++)
+{
+	fact=fact*i;
+}
+printf("%d",fact);
+}
+else
+printf("Factorial not defined");
 }
