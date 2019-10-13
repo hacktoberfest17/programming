2a98ca4b65645222a0be8d004f8736e4d5481f4b
vishakha0505 <53859055+vishakha0505@users.noreply.github.com>
2019-10-13 15:51:34 +0530

    Update Factorial.py

--- factorial/python/Factorial.py
+++ factorial/python/Factorial.py
@@ -1,29 +1,16 @@
-"""
+num = 7
 
-Script to generate Factorial in python 2.7 and 3
+# uncomment to take input from the user
+#num = int(input("Enter a number: "))
 
-"""
+factorial = 1
 
-result = [i for i in range(1000)]
-
-def fact_dp(n):
-    result[0] = 1
-    end = min(n, 1000) + 1
-    for i in range(1, end):
-        result[i] = i * result[i - 1]
-    
-    return result[n]
-
-if __name__ == '__main__':
-    
-    n = None
-    
-    try:
-        n = int(input("Enter a number :"))
-    except ValueError:
-        print("Not a number")
-
-    if (n <= 0):
-        print (1)
-    else:
-        print("Factorial : " + str(fact_dp(n)))
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
