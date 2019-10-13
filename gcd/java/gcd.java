fc195aafd29f68507fd1681431aff3157d614468
vishakha0505 <53859055+vishakha0505@users.noreply.github.com>
2019-10-13 15:42:58 +0530

    Update gcd.java

--- gcd/java/gcd.java
+++ gcd/java/gcd.java
@@ -1,27 +1,16 @@
-// Java program to find factorial of given number
-class gcd
-{
-    // Recursive function to return gcd of a and b
-    static int gcd(int a, int b)
-    {
-        // Everything divides 0 
-        if (a == 0 || b == 0)
-           return 0;
-      
-        // base case
-        if (a == b)
-            return a;
-      
-        // a is greater
-        if (a > b)
-            return gcd(a-b, b);
-        return gcd(a, b-a);
-    }
-     
-    // Driver method
-    public static void main(String[] args) 
-    {
-        int a = 98, b = 56;
-        System.out.println("GCD of " + a +" and " + b + " is " + gcd(a, b));
+public class GCD {
+    public static void main(String[] args) {
+        int n1 = 81, n2 = -153;
+        // Always set to positive
+        n1 = ( n1 > 0) ? n1 : -n1;
+        n2 = ( n2 > 0) ? n2 : -n2;
+        while(n1 != n2)
+        {
+            if(n1 > n2)
+                n1 -= n2;
+            else
+                n2 -= n1;
+        }
+        System.out.println("G.C.D = " + n1);
     }
 }
