d8ecfb50154c7b9e67579c62b5dd7eb88e0f326d
vishakha0505 <53859055+vishakha0505@users.noreply.github.com>
2019-10-13 16:21:40 +0530

    Update factorial.php

--- factorial/php/factorial.php
+++ factorial/php/factorial.php
@@ -1,14 +1,12 @@
-<?php
-/**
-* Calculate a factorial given a number.
-* For different results, change the function call.
-*/
-function factorial($number) { 
-    if ($number < 2) { 
-        return 1; 
-    } else { 
-        return ($number * factorial($number-1)); 
-    } 
-}
-$fact_res = factorial(10);
-echo $fact_res;
+<?php  
+$num = 4;  
+$factorial = 1;  
+for ($x=$num; $x>=1; $x--)   
+{  
+  $factorial = $factorial * $x;  
+}  
+echo "Factorial of $num is $factorial";  
+?>  
+Output:
+
+
