12f1d62444ee8391ae1e8af2c2304c7ba6f1c3fc
28Gaurav <iyourfriendgaurav@gmail.com>
2019-10-23 19:40:44 +0530

    Properly indented the code, corrected some mistakes. Added 'return 0.'

--- c_programming/multiples.c
+++ c_programming/multiples.c
@@ -1,19 +1,24 @@
-#include <stdio.h>
+#include <stdio.h>         		                           \\Including Header Files
+#include<conio.h>
 
-#define MAX 1000
+#define MAX 1000				                           \\Macros : Replaces variable names with designated values.
 #define THREE 3
 #define FIVE 5
 
-int main(){
+int main()
+{
 	int i;
-	int multiplesof3;
-	int multiplesof5;
+	int multiplesOf3;                                       \\Written in CamelCase type
+	int multiplesOf5;
 	int sum;
 
-	for(i = 1; i < 10; i++){
-		multiplesof3 = 3 * 3;
-		multiplesof5 = 5 * 5;
-		printf("3: %d \t 5: %d\n", multiplesof3, multiplesof5);
+	for(i = 1; i < 10; i++)
+	{
+		multiplesOf3 = THREE * THREE;                        \\THREE is replaced by 3, at time of execution.
+		multiplesOf5 = FIVE * FIVE;                          \\FIVE is replaced by 5, at time of execution.
+		printf("Multiple of 3: %d\n", multiplesOf3);
+		printf("Multiple of 5: %d\n", multiplesOf5);
 	}
 
+return 0;
 }
