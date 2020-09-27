2a87f286a5dfe640d1ab34c8852c732ff9ab9652
Theodosis Paidakis <theodosis_paid@yahoo.gr>
2020-09-27 22:11:37 +0300

    added solution to Peaks and Valleys problem

---
+++ Peaks_and_Valleys/peaksAndVallleys.cpp
@@ -0,0 +1,19 @@
+class BuyAndSellStocks
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        if (n == 1)
+            return 0;
+        int profit = 0;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (prices[i + 1] > prices[i])
+                profit += prices[i + 1] - prices[i];
+        }
+
+        return profit;
+    }
+};
