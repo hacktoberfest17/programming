6dbfd6a101f87d43fbb272303bc3d4940be3cc46
Kajori4 <48029674+Kajori4@users.noreply.github.com>
2020-10-01 18:06:35 +0530

    This is my different approach on quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,37 +1,57 @@
-def partition(myList, start, end):
-    pivot = myList[start]
-    left = start+1
-    # Start outside the area to be partitioned
-    right = end
-    done = False
-    while not done:
-        while left <= right and myList[left] <= pivot:
-            left = left + 1
-        while myList[right] >= pivot and right >=left:
-            right = right -1
-        if right < left:
-            done= True
-        else:
-            # swap places
-            temp=myList[left]
-            myList[left]=myList[right]
-            myList[right]=temp
-    # swap start with myList[right]
-    temp=myList[start]
-    myList[start]=myList[right]
-    myList[right]=temp
-    return right
+# Python program for implementation of Quicksort Sort
 
-def quicksort(myList, start, end):
-    if start < end:
-        # partition the list
-        split = partition(myList, start, end)
-        # sort both halves
-        quicksort(myList, start, split-1)
-        quicksort(myList, split+1, end)
-    return myList
+# This function takes last element as pivot, places
+# the pivot element at its correct position in sorted
+# array, and places all smaller (smaller than pivot)
+# to left of pivot and all greater elements to right
+# of pivot
 
-def main():
-    myList = [7,2,5,1,29,6,4,19,11]
-    sortedList = quicksort(myList,0,len(myList)-1)
-    print(sortedList)
+
+def partition(arr, low, high):
+	i = (low-1)		 # index of smaller element
+	pivot = arr[high]	 # pivot
+
+	for j in range(low, high):
+
+		# If current element is smaller than or
+		# equal to pivot
+		if arr[j] <= pivot:
+
+			# increment index of smaller element
+			i = i+1
+			arr[i], arr[j] = arr[j], arr[i]
+
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return (i+1)
+
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low --> Starting index,
+# high --> Ending index
+
+# Function to do Quick sort
+
+
+def quickSort(arr, low, high):
+	if len(arr) == 1:
+		return arr
+	if low < high:
+
+		# pi is partitioning index, arr[p] is now
+		# at right place
+		pi = partition(arr, low, high)
+
+		# Separately sort elements before
+		# partition and after partition
+		quickSort(arr, low, pi-1)
+		quickSort(arr, pi+1, high)
+
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+	print("%d" % arr[i]),
+
