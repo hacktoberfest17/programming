782d3cd27b2afa1d873cea3ec8d2bd38c5afdcaa
Sahil-Mandal15 <sahilmandal06@gmail.com>
2023-10-26 13:58:45 +0530

    Printing hello world in java

--- hello_world/java/World_Hello.java
+++ hello_world/java/World_Hello.java
@@ -2,8 +2,6 @@ package Programming.programming.hello_world.java;
 
 public class World_Hello {
     public static void main(String[] args) {
-
-
         System.out.println("HELLO WORLD");
     }
 }
